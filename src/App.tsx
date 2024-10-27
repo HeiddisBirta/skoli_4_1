3578baa87672157bf9d68566c591f70c974e6554
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2024-10-27 12:23:33 +0000

    added props to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="w-96 flex flex-col p-6">
-      <h1 className="p-6 flex justify-center">I like flowers</h1>
-      <Card>Card</Card>
+
+      <Card title="I like flowers">Card</Card>
       <p className="p-6 flex justify-center">
         A card with photo, title and small text
       </p>
