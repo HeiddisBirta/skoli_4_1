7acd16f47ca32d3fa971e64f994d30a031f3a7a7
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2024-10-27 12:00:44 +0000

    a card with photo, title and small text

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/1037994/pexels-photo-1037994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="flowers, pink backround"
+      />
+    </div>
+  );
+}
+
+export default Card;
